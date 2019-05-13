b1b4966e093bc0fcf66e673089af3533716ff7e7
Alankhoang <alankhoangfr@gmail.com>
2019-05-13 20:35:34 +0200

    Updates

---
+++ precache-manifest.536f399eb4b11d488e78ee39df303190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6148ea5d663d75557ae06a7f96c21a",
+    "url": "/trading/index.html"
+  },
+  {
+    "revision": "99c6fa8dd6f3eb6c353c",
+    "url": "/trading/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/trading/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "99c6fa8dd6f3eb6c353c",
+    "url": "/trading/static/js/main.6ef72985.chunk.js"
+  },
+  {
+    "revision": "b1cc8a40312a63b5e60b",
+    "url": "/trading/static/js/runtime~main.61b56d58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trading/static/media/logo.5d5d9eef.svg"
+  }
+]);
